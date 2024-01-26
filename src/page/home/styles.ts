4452ca3a426f49c637496e3a1ce92fa285365466
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-26 08:25:12 -0300

    styles: add style Home page

---
+++ src/page/home/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native"
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #121214;
+`
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #F1F1F1;
+`
