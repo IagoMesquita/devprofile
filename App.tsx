616a6b32a123db705bc9df67f4477a6c744cc931
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-26 09:53:16 -0300

    feat: creating theme global

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Home } from './src/page/home';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme';
 
 export default function App() {
   return (
-    <Home/>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    black: '#000',
+    white: '#FFF',
+    primary: '#EBA417',
+    primaryLight: '#FFDB77',
+    secondary: '#79C7FF', 
+    success: '#12A454',
+    danger:'#E83F5B',
+    dark: '#121214',
+    light: '#F1F1F1',
+    gray500: '#A8A8B3',
+    gray800: '#29292E'
+  }
+}
