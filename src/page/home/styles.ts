634566babf58e66d32a7a8dbeeea7b92d40ee84d
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-27 11:05:16 -0300

    style: add style home page

--- src/page/home/styles.ts
+++ src/page/home/styles.ts
@@ -1,14 +1,47 @@
-import styled from "styled-components/native"
+import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.dark};
+`;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(17)}px;
+
+  background-color: ${({ theme }) => theme.colors.secondary};
   justify-content: center;
+  align-self: center;
+  flex-direction: row;
+  padding-top: ${RFValue(40)}px;
+`;
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`;
+export const UserInfo = styled.View`
+  flex-direction: row;
   align-items: center;
-  background-color: ${({theme}) => theme.colors.dark};
-`
-export const Title = styled.Text`
-  font-family: ${({theme}) => theme.fonts.bold};
-  font-size: 24px;
-  font-weight: bold;
-  color: ${({theme}) => theme.colors.light};
-`
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity``;
+export const UserAvatar = styled.Image`
+  width: ${RFValue(52)}px;
+  height: ${RFValue(52)}px;
+  border-radius: 10px;
+`;
+export const UseInforDetail = styled.View`
+  margin-left: 17px;
+`;
+export const UserGreenting = styled.Text`
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+
