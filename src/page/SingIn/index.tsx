cc3fc32c896d41886b95fe8a825cc7fd525e647d
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-29 07:22:42 -0300

    feat: add ScrollView for closed keyboard

--- src/page/SingIn/index.tsx
+++ src/page/SingIn/index.tsx
@@ -1,15 +1,21 @@
+import { ScrollView } from "react-native";
 import { Input } from "../../components/Forms/Input";
 import { Container, Content, Title } from "./styles";
 
 
 export default function SingIn() {
   return (
-    <Container>
-      <Content>
-        <Title>Faça seu login</Title>
-        <Input placeholder="Email"/>
-        <Input placeholder="Senha"/>
-      </Content>
-    </Container>
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{ flex: 1 }}
+    >
+      <Container>
+        <Content>
+          <Title>Faça seu login</Title>
+          <Input placeholder="Email" />
+          <Input placeholder="Senha" />
+        </Content>
+      </Container>
+    </ScrollView>
   )
 }
