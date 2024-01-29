256ca4497f8fe9276966d35601f91d4b37e07d1f
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-29 06:53:25 -0300

    feat: initial SingIn

---
+++ src/page/SingIn/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Content, Title } from "./styles";
+
+
+export default function SingIn() {
+  return (
+    <Container>
+      <Content>
+        <Title>Faça seu login</Title>
+      </Content>
+    </Container>
+  )
+}
