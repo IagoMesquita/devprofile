3f0a89ce49fb3d60ede45cea92cd32668459e50d
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-02-03 10:21:16 -0300

    feat: add useForm and add props in InputContrls

--- src/page/SingIn/index.tsx
+++ src/page/SingIn/index.tsx
@@ -1,15 +1,29 @@
 import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
-import { Input } from '../../components/Forms/Input';
 import { Container, Content, CreateAccount, CreateAccountTitle, ForgotPasswordButtom, ForgotPasswordText, Icon, Logo, Title } from './styles';
+import { InputControl } from '../../components/Forms/InputControl';
 import { Buttom } from '../../components/Forms/Buttom';
 import logo from '../../assets/logo.png';
+
 import { useNavigation } from '@react-navigation/native';
+import { useForm, FieldValues } from "react-hook-form"
 
 interface ScreenNavigationProps {
   navigate: (screen: string) => void;
 }
 
+interface IFormData {
+  email: string;
+  password: string;
+}
+
 export default function SingIn() {
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FieldValues>({
+    defaultValues:{
+      email: "",
+      password: ""
+    }
+  })
 
   const navigation = useNavigation<ScreenNavigationProps>()
   return (
@@ -28,8 +42,10 @@ export default function SingIn() {
             <View>
               <Title>Faça seu login</Title>
             </View>
-            <Input placeholder="Email" />
-            <Input placeholder="Senha" />
+            {/* <Input placeholder="Email" /> */}
+            {/* <Input placeholder="Senha" /> */}
+            <InputControl control={control} name='Email' placeholder='Digite seu email' />
+            <InputControl control={control} name='Senha' placeholder='Digite seu senha' />
             <Buttom title="Entrar" />
             <ForgotPasswordButtom>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
