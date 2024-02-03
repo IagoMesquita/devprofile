7d566f1894edfa3b02dadc12d17d415ffe5f6a81
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-02-03 12:29:06 -0300

    refactor: Pagse SingUp with hook form

--- src/page/SingUp/index.tsx
+++ src/page/SingUp/index.tsx
@@ -3,15 +3,42 @@ import { Input } from "../../components/Forms/Input";
 import { BackToSingIn, BackToSingInTitle, Container, Content, Icon, Logo, Title } from "./styles";
 import { Buttom } from "../../components/Forms/Buttom";
 import logo from '../../assets/logo.png';
+
 import { useNavigation } from '@react-navigation/native';
+import { FieldValues, useForm } from "react-hook-form";
+import { InputControl } from "../../components/Forms/InputControl";
 
 interface ScreenNavigationProps {
   goBack: () => void;
 }
 
+interface IFormInputs {
+  [name: string]: any;
+}
+
 
 export default function SingUp() {
   const navigation = useNavigation<ScreenNavigationProps>()
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      name: "",
+      password: ""
+    }
+  })
+
+
+  const handleSingUp = (form: IFormInputs) => {
+    const data = {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    } 
+
+    console.log(data)
+  };
+
   return (
     <KeyboardAvoidingView
       // enabled
@@ -24,14 +51,26 @@ export default function SingUp() {
       >
         <Container>
           <Content>
-            <Logo source={logo}/>
+            <Logo source={logo} />
             <View>
               <Title>Crie sua conta</Title>
             </View>
-            <Input placeholder="Nome completo" />
-            <Input placeholder="Email" />
-            <Input placeholder="Senha" />
-            <Buttom title="Criar conta" />
+            <InputControl control={control} name='name' placeholder='Digite seu nome'
+              autoCapitalize='none'
+              autoCorrect={false}
+              keyboardType='email-address'
+            />
+            <InputControl control={control} name='email' placeholder='Digite seu email'
+              autoCapitalize='none'
+              autoCorrect={false}
+              keyboardType='email-address'
+            />
+            <InputControl control={control} name='password' placeholder='Digite seu senha'
+              autoCapitalize='none'
+              autoCorrect={false}
+              secureTextEntry
+            />
+            <Buttom title="Criar conta"  onPress={handleSubmit(handleSingUp)}/>
           </Content>
         </Container>
       </ScrollView>
