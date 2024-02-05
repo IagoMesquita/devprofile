140f8c139c4012c1f64005d925e04958e0231512
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-02-05 07:17:11 -0300

    feat: add componet Error

--- src/components/Forms/InputControl/index.tsx
+++ src/components/Forms/InputControl/index.tsx
@@ -2,17 +2,19 @@ import { FunctionComponent } from 'react';
 import { TextInputProps } from 'react-native';
 import { Control, Controller } from 'react-hook-form';
 
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 import { Input } from '../Input';
 
 interface InputProps extends TextInputProps {
   control: Control,
   name: string,
+  error: string | undefined
 }
 
 export const InputControl: FunctionComponent<InputProps> = ({
   control,
   name,
+  error,
   ...otherProps
 }) => {
   return (
@@ -29,6 +31,7 @@ export const InputControl: FunctionComponent<InputProps> = ({
         )}
         name={name}
       />
+      {error && <Error>{error}</Error>}
     </Container>
   )
 }
--- src/components/Forms/InputControl/styles.ts
+++ src/components/Forms/InputControl/styles.ts
@@ -4,4 +4,10 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   width: 100%;
-`
+`;
+export const Error = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.danger};
+  margin-bottom: 16px;
+`;
