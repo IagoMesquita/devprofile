3133fdac6c85d82c1e592517bf8b98adbcd2870e
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-02-14 09:13:35 -0300

    feat: add authenticated route

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { Home } from "../page/Home";
+import SingUp from "../page/SingUp";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName="Home"
+    >
+      <Stack.Screen name="Home" component={Home}/>
+    </Stack.Navigator>
+  )
+} 
