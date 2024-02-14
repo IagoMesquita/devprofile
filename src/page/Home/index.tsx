bf941f02925b32ea70f525c101f105b2d07017ca
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-02-14 09:58:45 -0300

    feat: add alert in logout

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -13,9 +13,24 @@ import {
 } from './styles';
 import avatarDefault from '../../assets/avatar02.png'
 import { useAuth } from '../../Hooks/useAuth';
+import { Alert } from 'react-native';
 
 export function Home() {
   const {user, singOut} = useAuth();
+
+  const handleSingOut = () => {
+    Alert.alert('Tem certeza?', 'Deseja realmente sair?', [
+      {
+        text: 'Cancelar',
+        onPress: () => {}
+      },
+      {
+        text: 'Sair',
+        onPress: () => singOut()
+      }
+    ])
+  };
+
   return (
     <Container>
       <Header>
@@ -32,7 +47,7 @@ export function Home() {
               <UserName>{user.name}</UserName>
             </UseInforDetail>
           </UserInfo>
-          <LogoutButton onPress={singOut}>
+          <LogoutButton onPress={handleSingOut}>
             <Icon name='power' />
           </LogoutButton>
         </UserWrapper>
